87fcdf6a0c64cd27c7360aba1ad747469eb55f6f
Jared Stilwell <jstilwell@rmn.com>
2016-06-01 13:02:50 -0500

    Migrated the code to use standard formatting

--- test/fixtures/jscale.js
+++ test/fixtures/jscale.js
@@ -1,31 +1,28 @@
-var log = require( 'npmlog' );
-var zlib = require( 'zlib' );
-var UglifyJS = require("uglify-js");
+var zlib = require('zlib')
+var UglifyJS = require('uglify-js')
 
-function scaleData ( input, shouldUglify ) {
-    var sizes = {
-        untransformed: input.length
-    };
-    var gzipped = zlib.gzipSync( input );
-    sizes.gzipped = gzipped.length;
+function scaleData (input, shouldUglify) {
+  var sizes = {
+    untransformed: input.length
+  }
+  var gzipped = zlib.gzipSync(input)
+  sizes.gzipped = gzipped.length
 
-
-    if ( shouldUglify ) {
-        try {
-            var uglified = UglifyJS.minify(input, {fromString: true}).code;
-            sizes.uglified = uglified.length;
-            var uglifiedAndGzipped = zlib.gzipSync( uglified );
-            sizes.uglifiedAndGzipped = uglifiedAndGzipped.length;
-        } catch ( e ) {
-            throw new Error(
-                'UglifyJS choked on this file. Are you sure it\'s a JavaScript file? ' +
-                'You cannot uglify a non-JS file.\n' + e.message + '\n' + e.stack
-            );
-            process.exit( 1 );
-        }
+  if (shouldUglify) {
+    try {
+      var uglified = UglifyJS.minify(input, {fromString: true}).code
+      sizes.uglified = uglified.length
+      var uglifiedAndGzipped = zlib.gzipSync(uglified)
+      sizes.uglifiedAndGzipped = uglifiedAndGzipped.length
+    } catch (e) {
+      throw new Error(
+        "UglifyJS choked on this file. Are you sure it's a JavaScript file? " +
+        'You cannot uglify a non-JS file.\n' + e.message + '\n' + e.stack
+      )
     }
+  }
 
-    return sizes;
+  return sizes
 }
 
-module.exports = scaleData;
+module.exports = scaleData
